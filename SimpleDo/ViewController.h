919d0398a9edbc024d886d261ec6185349dd4589
gukong <1239242238@qq.com>
2015-06-18 22:31:37 +0800

    Initial Commit

---
+++ SimpleDo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleDo
+//
+//  Created by gukong on 15/6/18.
+//  Copyright (c) 2015年 gukong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleDo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleDo
+//
+//  Created by gukong on 15/6/18.
+//  Copyright (c) 2015年 gukong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
